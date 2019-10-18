77d189721e67f746934fe673069a3ac0bf7c4656
David <dwarrenfive@gmail.com>
2019-10-17 17:19:50 -0700

    two working api fetching quotes

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,49 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: [],
-            isLoading: false
+            kQuotes: [],
+            tQuotes: [],
+            isLoading: true,
         }
     }
 
-
-    componentDidMount() {
-        this.fetchQuote()
-    }
-
-    fetchQuote() {
+    fetchKanye() {
         fetch("https://api.kanye.rest")
             .then(responce => responce.json())
-            .then(parsedJSON => console.log(parsedJSON))
+            .then(parsedJSON => parsedJSON)
+            .then(kQuotes => this.setState({
+                kQuotes: kQuotes,
+                isLoading: false
+            }))
+            .catch(error => console.log('parsing failed', error))
+    }
+
+    fetchTrump() {
+        fetch("https://api.whatdoestrumpthink.com/api/v1/quotes/random")
+            .then(responce => responce.json())
+            .then(parsedJSON => parsedJSON)
+            .then(sQuotes => this.setState({
+                tQuotes: sQuotes,
+                isLoading: false
+            }))
             .catch(error => console.log('parsing failed', error))
     }
 
     render() {
-        const { isLoading, items } = this.state;
+        const { tQuotes, kQuotes, isLoading } = this.state;
         return (
             <div className="container">
-                <h1 className='text-center text-white m-2'>Famous Kanye Quotes told by Master Yoda!</h1>
-                <div>
-                    <img src="./yoda.png" className="z-index:2 m-2" title="When 900 years you reach, look as good you will not." />
-                </div>
+                <h1 className='text-center text-white m-2'>Trump & Kanye</h1>
+                <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939" className="z-index:2 m-2 rounded mx-auto d-block" />
                 <div className="col text-center">
-                    <button className="btn btn-success m-2">Action!</button>
+                    <button onClick={() => { this.fetchTrump(); this.fetchKanye(); }} className="btn btn-success m-2">Action!</button>
                 </div>
-            </div>
+                <div className='mt-2'>
+                    {
+                        !isLoading ? <div><h6>Trump!</h6><p>{tQuotes.message}</p><br /><h6>Kanye!</h6><p>{kQuotes.quote}</p></div> : null
+                    }
+                </div>
+            </div >
         )
     }
 }
